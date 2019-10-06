c3c8a5146e0c52357ebdb5f07b3c2470df25a12e
Maciej Kuśnierz <>
2019-10-06 17:22:40 +0200

    Use API hosted at GCloud

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
 
 export function getCustomers() {
-    return fetch(`customers.json`)
+    return fetch(`https://customers-api-253714.appspot.com/getCustomers`)
       .then(response => response.json())
 }
 
 export function getCustomer(id) {
-    return fetch(`../customers.json`)
+    return fetch(`https://customers-api-253714.appspot.com/getCustomer?id=${id}`)
         .then(response => response.json())
-        .then(response => response.find(customer => {return customer.id == id}))
 }
